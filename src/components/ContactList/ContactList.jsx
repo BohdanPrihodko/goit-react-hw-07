c3be0b1cef344dab3abbd284252ceefe8a466582
BohdanPrihodko <157399457+BohdanPrihodko@users.noreply.github.com>
2024-08-31 13:58:18 +0300

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from "react-redux";
+import { deleteContact, selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
+import Contact from "../Contact/Contact.jsx";
+import styles from "./ContactList.module.css";
+
+const ContactList = () => {
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+  const dispatch = useDispatch();
+
+  const visibleContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <ul className={styles.contactList}>
+      {visibleContacts.map(({ id, name, number }) => (
+        <Contact
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          onDeleteContact={() => dispatch(deleteContact(id))}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ContactList;
